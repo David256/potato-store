b454b093ff805c7286ed34c2a85acad25f5f5058
David256 <davidborja456@gmail.com>
2021-03-26 16:50:59 -0500

    Component to cart

---
+++ src/components/CartInline.vue
@@ -0,0 +1,17 @@
+<template>
+  <button type="button" class="btn btn-light">
+    <img src="../assets/shopping-cart.svg" alt="">
+    Cart
+    <span class="badge bg-danger">{{ productsInCart.length }}</span>
+  </button>
+</template>
+
+<script>
+import Vuex from 'vuex';
+
+export default {
+  computed: {
+    ...Vuex.mapState(['productsInCart']),
+  },
+};
+</script>
