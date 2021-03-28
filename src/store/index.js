6328a8ae50e91658add064fee16d15d284c895bd
David256 <davidborja456@gmail.com>
2021-03-28 14:01:10 -0500

    Update store

--- src/store/index.js
+++ src/store/index.js
@@ -2,15 +2,30 @@ import { createStore } from 'vuex';
 
 export default createStore({
   state: {
+    carousel: [],
     products: [],
     productsInCart: [],
   },
   mutations: {
+    updateCarousel(state, carousel) {
+      state.carousel = carousel;
+    },
     updateProducts(state, products) {
       state.products = products;
     },
   },
   actions: {
+    async loadCarousel(context) {
+      const data = await fetch('db/carousel.json');
+
+      /**
+       * @type { Array }
+       */
+      const carousel = await data.json();
+
+      context.commit('updateCarousel', carousel);
+      console.log(carousel.length, 'carousel loaded');
+    },
     async loadProducts(context) {
       const data = await fetch('db/products.json');
 
@@ -20,7 +35,6 @@ export default createStore({
       const products = await data.json();
 
       context.commit('updateProducts', products);
-
       console.log(products.length, 'products loaded');
     },
   },
